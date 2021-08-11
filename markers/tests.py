fdfbe689f26a2347e01e7b82860f731076b21e00
SoyJoseC <ckmendoza3198@gmail.com>
2021-08-11 14:17:33 -0500

    Writing the first test

--- markers/tests.py
+++ markers/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
 # Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        a = 5
+        b = 10
+    def tearDown(self):
+        a,b= 0
+    def test_Maps(self):
+        return self.assertEqual(self.a+ self.b, 15 )
